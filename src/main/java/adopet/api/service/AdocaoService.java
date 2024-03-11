74a0e881d7eb24f4589e386589679f3190f5c947
matheus mozart <mmsnborges@gmail.com>
2024-03-11 12:49:56 -0400

    adicionando  várias exceções

--- src/main/java/adopet/api/service/AdocaoService.java
+++ src/main/java/adopet/api/service/AdocaoService.java
@@ -37,6 +37,17 @@ public class AdocaoService {
 
     public void solicitar(SolicitacaoDeAdocaoDTO dto){
         Pet pet = petRepository.getReferenceById(dto.idPet());
+        if(pet.getAdotado()){
+            throw new IllegalStateException("Pet já adotado");
+        }
+        Boolean petStatusAdocaco = adocaoRepository.existsByPetIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO);
+        if(petStatusAdocaco){
+            throw new UnsupportedOperationException("Pet com adoção em andamento.");
+        }
+        Integer tutoAdocacoes = adocaoRepository.countByTutorIdAndStatus(dto.idTutor(), StatusAdocao.APROVADO);
+        if (tutoAdocacoes == 2){
+                throw new IllegalStateException("Tuto com o máximo adoções.");
+        }
         Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
 
         adocaoRepository.save(new Adocao(tutor,pet, dto.motivo()));
